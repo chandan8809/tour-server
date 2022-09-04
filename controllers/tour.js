7edc8b53208f9711a8ab8b07115d87a5bbd15237
Chandan kumar <chandan.kumar@apnamart.in>
2022-09-05 00:04:15 +0530

    tour route done

--- controllers/tour.js
+++ controllers/tour.js
@@ -4,7 +4,9 @@ import TourModal from "../models/tour.js";
 export const createTour = async(req,res)=>{
     const tour=req.body;
     const newTour= new TourModal({
-        ...tour, createdAt:new Date().toISOString()
+        ...tour,
+        creator:req.userId,
+        createdAt:new Date().toISOString()
 
     })
 
@@ -21,6 +23,17 @@ export const getTours=async(req,res)=>{
     try{
         const tours =await TourModal.find()
         res.status(200).json(tours)
+    }
+    catch(error){
+        res.status(404).json({message:"something went wrong"})
+    }
+}
+
+export const getTour=async(req,res)=>{
+    const {id}=req.params;
+    try{
+        const tour =await TourModal.findById(id)
+        res.status(200).json(tour)
     }
     catch(error){
         res.status(404).json({message:"something went wrong"})
--- routes/tour.js
+++ routes/tour.js
@@ -1,9 +1,11 @@
 import express  from "express";
 const router =express.Router()
+import auth from "../middleware/auth.js"
 
-import { createTour,getTours } from "../controllers/tour.js";
+import { createTour,getTours ,getTour} from "../controllers/tour.js";
 
-router.post("/",createTour)
+router.post("/",auth,createTour)
 router.get("/",getTours)
+router.get("/:id",getTour)
 
 export default router
